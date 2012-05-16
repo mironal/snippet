a5fb278604330c95356bc26cc45f985019f771ca
miro <miro.gunp@gmail.com>
2012-05-16 15:09:08 +0900

    modified:   print_before.c     fix indent

--- c/lesson/function/print_before.c
+++ c/lesson/function/print_before.c
@@ -7,15 +7,15 @@ int main(int argc, char* argv[])
 
     hoge = 1;
     fuga = 4;
-    printf("a = %d, b = %d : a + b = %d\n",hoge, fuga, hoge + fuga);
+    printf("a = %d, b = %d : a + b = %d\n", hoge, fuga, hoge + fuga);
 
     hoge = 4;
     fuga = 5;
-    printf("a = %d, b = %d : a + b = %d\n",hoge, fuga, hoge + fuga);
+    printf("a = %d, b = %d : a + b = %d\n", hoge, fuga, hoge + fuga);
 
     hoge = 8;
     fuga = 1;
-    printf("a = %d, b = %d : a + b = %d\n",hoge, fuga, hoge + fuga);
+    printf("a = %d, b = %d : a + b = %d\n", hoge, fuga, hoge + fuga);
 
     return 0;
 }
