9b057a5b2f094e9a078a159b52a97d10ccc4acb2
miro <miro.gunp@gmail.com>
2012-05-16 12:29:35 +0900

    new file:   printf.c

---
+++ c/lesson/hello/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    printf("int = %d, long = %ld\n",100,5000000000);
+
+    printf("float = %f, double = %f\n",10.5,10000.5);
+
+    return 0;
+}
