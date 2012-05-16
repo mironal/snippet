409becffc4680d1d77b22499d82101bc3edd293f
miro <miro.gunp@gmail.com>
2012-05-16 19:28:41 +0900

    new file:   string.c

---
+++ c/lesson/string/string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    char str[128];
+
+    /* memset(&str[0], 0, 128); */
+
+    /*
+    str[0] = 'a';
+    str[1] = 'b';
+    */
+
+    /*
+    str[3] = 'd';
+    str[4] = 'e';
+    */
+
+    printf("%s\n",str);
+
+    return 0;
+}
